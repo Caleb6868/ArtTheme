54754dd5bea16f358eede6d2f0dc67ec9b48fb53
Caleb6868 <herman2108@students.d211.org>
2024-09-19 21:19:17 -0500

    finished project and decorated.

--- app/src/main/java/com/example/arttheme/MainActivity.kt
+++ app/src/main/java/com/example/arttheme/MainActivity.kt
@@ -5,8 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,26 +15,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentComposer
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,53 +50,61 @@ override fun onCreate(savedInstanceState: Bundle?) {
         }
     }
 }
-
-
-
-
-
-
 @Composable
 fun ArtThemeLayout() {
+    var state by remember { mutableIntStateOf(0) }
+    val count: Array<String> =  stringArrayResource(R.array.titles)
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFB1DFF2)),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+
     ) {
 
-        ComposeTextAndImage(1)
+        ComposeTextAndImage(state)
 
         Spacer(modifier = Modifier.size(40.dp))
 
         Row {
+            Button(
 
-            ComposeButton()
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF841818),
+                    contentColor = Color.White
+                ),
+                onClick = { if (state > 0) {
+                    state--
+                } else if (state == 0){
+                    state = count.size - 1
+                } },
+            ) {
+                Text(text = "Back",
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+            Spacer(modifier = Modifier.size(100.dp))
+            Button(
+
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF2A8418),
+                    contentColor = Color.White
+                ),
+                onClick = { if (state < count.size - 1) {
+                    state++
+                } else if (state == count.size - 1){
+                    state = 0
+                }},
+            ) {
+                Text(text = "Next",
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun ComposeButton() {
-    var state by remember { mutableStateOf(0) }
-    Button(
-
-        onClick = { if (state > 0) {
-            state--
-        } },
-    ) {
-
-
-    }
-    Spacer(modifier = Modifier.size(100.dp))
-    Button(
-        onClick = { if (state < 6) {
-            state++
-            ComposeTextAndImage(currentStep = state)
-        } },
-    ) {
-
-
     }
 }
 
@@ -106,17 +112,31 @@ fun ComposeButton() {
 fun ComposeTextAndImage(currentStep: Int) {
     val stringTitles: Array<String> =  stringArrayResource(R.array.titles)
     val stringDesc: Array<String> =  stringArrayResource(R.array.descriptions)
+    val imageArray = arrayOf(
+        R.drawable.adler_planetarium,
+        R.drawable.art_institute,
+        R.drawable.chinatown,
+        R.drawable.melenium_park,
+        R.drawable.navy_pier,
+        R.drawable.sears_tower,
+        R.drawable.shedd_aquarium,
+        R.drawable.wrigley_field
+    )
     Image(
         modifier = Modifier
             .height(200.dp)
-            .fillMaxWidth(),
-        painter = painterResource(id = R.drawable.art_institute),
+            .fillMaxWidth()
+            .background(Color(0xFF57C5F4))
+            .border(width = 4.dp, color = Color.Black, shape = RoundedCornerShape(8.dp))
+                .padding(2.dp),
+        painter = painterResource(imageArray[currentStep]),
         contentDescription = null
     )
     Spacer(modifier = Modifier.size(30.dp))
     Text(
         text = stringTitles[currentStep],
-        fontSize = 30.sp
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
     )
     Text (
         text = stringDesc[currentStep]
